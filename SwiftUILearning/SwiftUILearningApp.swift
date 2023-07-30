d6d5eef21cdd5a3d8b3424bf3ec33412ec530c2d
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-30 16:56:25 +0300

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Amr El-Fiqi on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
