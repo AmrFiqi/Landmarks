8b928a2c909494438c9879ce9bf9e479b979a690
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-30 17:49:43 +0300

    learnt about SwiftUI Inspector

--- SwiftUILearning/ContentView.swift
+++ SwiftUILearning/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Fly High")
+                .font(.title)
+                .padding(10)
         }
         .padding()
     }
