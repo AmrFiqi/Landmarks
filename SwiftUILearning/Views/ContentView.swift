bd581547555671533b03ff73eac470f0c24391a0
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-31 12:24:30 +0300

    created the list to be populated dynamically

--- SwiftUILearning/Views/ContentView.swift
+++ SwiftUILearning/Views/ContentView.swift
@@ -9,36 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-                .ignoresSafeArea(edges: .top)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree National park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                Divider()
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Description text is to be used here")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-            .padding()
-            Spacer()
-        }
+        LandmarkList()
     }
     
 }
