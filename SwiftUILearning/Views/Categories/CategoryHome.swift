846cf12df855f7af4490de990b4f69e8919387ed
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-08-03 19:26:32 +0300

    created a category view and enums for it in the model

---
+++ SwiftUILearning/Views/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  SwiftUILearning
+//
+//  Created by Amr El-Fiqi on 03/08/2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+        
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
