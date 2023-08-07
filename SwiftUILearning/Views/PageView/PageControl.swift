4ec8f7355275502b5610a2ca5c2f5f5040600039
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-08-07 20:37:52 +0300

    Added coordinator for when the user click on the image to move the page back and forth

--- SwiftUILearning/Views/PageView/PageControl.swift
+++ SwiftUILearning/Views/PageView/PageControl.swift
@@ -15,11 +15,30 @@ struct PageControl: UIViewRepresentable {
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
         return control
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
     
     func updateUIView(_ uiView: UIPageControl, context: Context) {
         uiView.currentPage = currentPage
+    }
+    
+    // MARK: - Class Coordinator
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
     }
 }
 
